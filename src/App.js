e19fe595ea1017c9785c7aa668aaea5c7d17daf6
Gabriel Borges <gabrielbm06@gmail.com>
2018-05-20 17:17:40 -0300

    start with react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header'
+import Main from './components/Main'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Pet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Pet extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>  p e t s </h1>
+        </div>
+      );
+    }
+  }
+
+  export default Pet;
